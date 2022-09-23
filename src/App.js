065f73a11d534bca589e418d8bd52c995d095f29
Abzeejay <abiodun.ogunjemilua@gmail.com>
2022-09-23 12:09:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import BreadCrumbs from "./components/breadCrumbs/BreadCrumbs";
+import Header from "./components/header/Header";
+import Circle from "./components/circle/Circle";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <BreadCrumbs />
+      <Circle />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/employeeData.js
@@ -0,0 +1,53 @@
+import avatar1 from "./asset/avatar1.jpg";
+import avatar2 from "./asset/avatar2.jpg";
+import avatar3 from "./asset/avatar3.jpg";
+import avatar4 from "./asset/avatar4.jpg";
+import avatar5 from "./asset/avatar5.jpg";
+
+export const employeeData = [
+  {
+    employee: "Logan Herderson",
+    email: "logan@company.com",
+    lastLogin: "September 20, 2019",
+    timeFrame: "2 days ago",
+    department: "Accounting",
+    img: avatar1,
+    status: "Active",
+  },
+  {
+    employee: "Susie Calson",
+    email: "susie@company.com",
+    lastLogin: "September 20, 2019",
+    timeFrame: "5 hours ago",
+    department: "Development",
+    img: avatar2,
+    status: "Inactive",
+  },
+  {
+    employee: "Markus Benes",
+    email: "Markus@company.com",
+    lastLogin: "September 20, 2019",
+    timeFrame: "4 days ago",
+    department: "Development",
+    img: avatar3,
+    status: "Active",
+  },
+  {
+    employee: "Marie Stephens",
+    email: "marie@company.com",
+    lastLogin: "September 20, 2019",
+    timeFrame: "2 days ago",
+    department: "Human Resources",
+    img: avatar4,
+    status: "Active",
+  },
+  {
+    employee: "Jacob Gibson",
+    email: "jacob@company.com",
+    lastLogin: "September 20, 2019",
+    timeFrame: "3 hours ago",
+    department: "Sales",
+    img: avatar5,
+    status: "Active",
+  },
+];
